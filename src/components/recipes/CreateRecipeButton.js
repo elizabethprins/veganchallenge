e064dc1c0db392ea79785997a7bb3473431e8e34
Bandi017 <andrei.burichita@gmail.com>
2017-07-06 09:34:23 +0200

    changed add recipe button icon

--- src/components/recipes/CreateRecipeButton.js
+++ src/components/recipes/CreateRecipeButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import RaisedButton from 'material-ui/RaisedButton'
-import StarIcon from 'material-ui/svg-icons/action/favorite'
+import StarIcon from 'material-ui/svg-icons/communication/import-contacts'
 import './CreateRecipeButton.css'
 
 class CreateRecipeButton extends PureComponent {
